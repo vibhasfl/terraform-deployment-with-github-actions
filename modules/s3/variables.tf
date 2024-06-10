e9f001ba5510435893165216783879cf56a879da
Vibhas Bhingarde <vibhas.fl@gmail.com>
2024-06-10 14:34:57 +0530

    Changed bucket encryption

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,10 +1,16 @@
 variable "s3bucketname" {
-  type = string
+  type        = string
   description = "Name of the s3 bucket"
 }
 
 variable "tags" {
-  type = map(string)
+  type        = map(string)
   description = "Resource tags"
   default     = {}
+}
+
+variable "s3bucketversioning" {
+  type        = string
+  description = "Bucket versioning"
+  default     = "Enabled"
 }
